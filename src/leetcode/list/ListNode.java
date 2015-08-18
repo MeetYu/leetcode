0c4b3b56a3118e883ab1787742eae994911efcb5
Meet Yu <ychao2014@gmail.com>
2015-08-18 20:15:19 +0800

    添加ListNode类

---
+++ src/leetcode/list/ListNode.java
@@ -0,0 +1,13 @@
+package leetcode.list;
+
+/**
+ * Created by meet on 2015/8/18.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
