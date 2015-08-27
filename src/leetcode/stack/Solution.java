bd0e7f567dc1bfe272bc17bcf15fc821f6970314
Meet Yu <ychao2014@gmail.com>
2015-08-27 08:51:36 +0800

    Add comments to isValid(String) method.

--- src/leetcode/stack/Solution.java
+++ src/leetcode/stack/Solution.java
@@ -8,6 +8,18 @@ import java.util.Stack;
  * Created by meet on 2015/8/26.
  */
 public class Solution {
+
+    /**
+     *
+     * leetcode: Valid Parentheses.
+     *
+     * Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+     * determine if the input string is valid.The brackets must close in the correct
+     * order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+     *
+     * @param s a string
+     * @return true if the string is valid, otherwise false.
+     */
     public boolean isValid(String s) {
         if (s == null || s.length() == 0)
             return false;
